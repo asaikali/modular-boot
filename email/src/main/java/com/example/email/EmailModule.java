1536bec6bfa2f245b3cbdc248343287c1022812b
Adib Saikali <asaikali@gmail.com>
2019-12-23 08:47:07 -0500

    Make the module classes be @Configuration classes

--- email/src/main/java/com/example/email/EmailModule.java
+++ email/src/main/java/com/example/email/EmailModule.java
@@ -1,13 +1,20 @@
 package com.example.email;
 
 import com.example.Module;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-@Component
+@Configuration
 public class EmailModule implements Module {
 
   @Override
   public String getName() {
     return "email";
+  }
+
+  @Bean
+  public EmailService emailService(JdbcTemplate jdbcTemplate) {
+    return new EmailService(jdbcTemplate);
   }
 }
--- email/src/main/java/com/example/email/EmailService.java
+++ email/src/main/java/com/example/email/EmailService.java
@@ -1,15 +1,11 @@
 package com.example.email;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Service;
 
-@Service
 public class EmailService {
 
   private final JdbcTemplate jdbcTemplate;
 
-  @Autowired
   public EmailService(JdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
   }
